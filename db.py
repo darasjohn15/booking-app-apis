81bbb7f91dcc3d3a78d7dd269b5c5ff8441034b2
Daras Johnson <darasjohnson@MacBook-Pro-2.local>
2025-06-30 17:15:27 -0400

    Additional File Changes

--- db.py
+++ db.py
@@ -7,6 +7,7 @@ def get_db_connection():
         host=os.getenv("DB_HOST"),
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT", 5432)
     )
     return conn
